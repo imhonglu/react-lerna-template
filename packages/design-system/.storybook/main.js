238558ce4bf1fd2de6dca8c64b0b16638dae9b58
imhonglu <anonymouser.unknown@gmail.com>
2021-04-08 17:39:21 +0900

    chore: storybook webpack 에 babel rootMode 적용

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -3,6 +3,10 @@ const path = require('path');
 module.exports = {
     stories: ['../src/**/*stories.mdx', '../src/**/*stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    babel: async (options) => ({
+        ...options,
+        rootMode: 'upward',
+    }),
     typescript: {
         reactDocgen: 'react-docgen-typescript',
     },
