0326bec39ab6130af2297d97fe8246f0033f2869
imhonglu <anonymouser.unknown@gmail.com>
2021-04-08 17:38:28 +0900

    chore: emotion 패키지 추가, eslint 설정 업데이트
    - emotion 설치, @emotion/babel-preset-css-prop 적용
    - eslint sort 관련 플러그인 추가
      - eslint-plugin-simple-import-sort
      - eslint-plugin-sort-destructure-keys
      - eslint-plugin-sort-keys-fix
      - eslint-plugin-typescript-sort-keys
    - eslint rule 처리시 error 를 2 로 통일

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,38 @@
 module.exports = {
-    root: true,
+    extends: ['airbnb-typescript-prettier'],
     parserOptions: {
-        tsconfigRootDir: __dirname,
         project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
+        tsconfigRootDir: __dirname,
     },
-    extends: 'airbnb-typescript-prettier',
+    plugins: [
+        'simple-import-sort',
+        'sort-keys-fix',
+        'sort-destructure-keys',
+        'typescript-sort-keys',
+    ],
+    root: true,
     rules: {
         // 취향대로 변경하세요.
-        indent: 0,
         'import/extensions': 0,
         'import/no-unresolved': 0,
-        'react/jsx-indent': ['error', 4],
-        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        'react/jsx-props-no-spreading': 0,
+        'react/jsx-sort-props': 2,
         'react/prop-types': 0,
         'react/self-closing-comp': [
-            'error',
+            2,
             {
                 component: true,
                 html: true,
             },
         ],
-        'react/jsx-props-no-spreading': 0,
-        'react-hooks/exhaustive-deps': 0,
+        'react/sort-comp': 2,
+        'simple-import-sort/exports': 2,
+        'simple-import-sort/imports': 2,
+        'sort-destructure-keys/sort-destructure-keys': 2,
+        'sort-keys-fix/sort-keys-fix': 1,
+        'typescript-sort-keys/interface': 2,
+        'typescript-sort-keys/string-enum': 2,
     },
 };
