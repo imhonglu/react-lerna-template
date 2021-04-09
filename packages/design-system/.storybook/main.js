7e31b6382952e8320dea8e3d72f3e890be85f2bd
imhonglu <anonymouser.unknown@gmail.com>
2021-04-09 19:33:51 +0900

    chore: storybook 용 alias 주입 및 lint 적용

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -1,13 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-param-reassign */
 const path = require('path');
 
 module.exports = {
-    stories: ['../src/**/*stories.mdx', '../src/**/*stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
     babel: async (options) => ({
         ...options,
         rootMode: 'upward',
     }),
+    stories: ['../src/**/*stories.mdx', '../src/**/*stories.@(js|jsx|ts|tsx)'],
     typescript: {
         reactDocgen: 'react-docgen-typescript',
+    },
+    webpackFinal: async (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '~': path.resolve(__dirname, '../src'),
+        };
+        return config;
     },
 };
