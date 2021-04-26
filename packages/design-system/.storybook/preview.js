7febf7e8ba017c0efdc5b739ecbe052d66bdb004
imhonglu <anonymouser.unknown@gmail.com>
2021-04-26 16:31:29 +0900

    chore: 사용하지 않는 스토리북 controls 규칙 제거

--- packages/design-system/.storybook/preview.js
+++ packages/design-system/.storybook/preview.js
@@ -5,12 +5,6 @@ import { GlobalStyles } from '../src/global-styles';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
-        },
-    },
 };
 
 export const decorators = [
