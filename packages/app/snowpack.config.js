584a6518f1ceb08b9cb5486c78ee759eb449d1f6
imhonglu <anonymouser.unknown@gmail.com>
2021-04-26 16:30:25 +0900

    chore: 빌드 결과물의 디렉토리 명을 dist 로 변경, 설정 간소화, lint 적용

--- packages/app/snowpack.config.js
+++ packages/app/snowpack.config.js
@@ -1,31 +1,16 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 
 module.exports = {
-    workspaceRoot: '../../',
+    buildOptions: {
+        out: 'dist',
+    },
+    devOptions: {
+        /* ... */
+    },
     mount: {
-        src: { url: '/dist' },
-        public: { url: '/', static: true },
+        public: '/',
+        src: '/dist',
     },
-    alias: {
-        '~': './src',
-    },
-    plugins: [
-        '@snowpack/plugin-react-refresh',
-        [
-            '@snowpack/plugin-babel',
-            {
-                transformOptions: {
-                    // babel transform options
-                    // configFile: path.resolve('../../', '.babelrc'),
-                    rootMode: 'upward',
-                },
-            },
-        ],
-    ],
-    routes: [
-        /* Enable an SPA Fallback in development: */
-        { match: 'routes', src: '.*', dest: '/index.html' },
-    ],
     optimize: {
         /* Example: Bundle your final build: */
         // "bundle": true,
@@ -33,10 +18,20 @@ module.exports = {
     packageOptions: {
         /* ... */
     },
-    devOptions: {
-        /* ... */
-    },
-    buildOptions: {
-        /* ... */
-    },
+    plugins: [
+        [
+            '@snowpack/plugin-babel',
+            {
+                transformOptions: {
+                    rootMode: 'upward',
+                },
+            },
+        ],
+        '@snowpack/plugin-react-refresh',
+    ],
+    routes: [
+        /* Enable an SPA Fallback in development: */
+        { dest: '/index.html', match: 'routes', src: '.*' },
+    ],
+    workspaceRoot: '../../',
 };
